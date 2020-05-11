ab5ffba2c2ee3fab6f0caa135f856e999d48d2ab
Heaan <cxy.eml@live.cn>
2020-05-11 16:08:00 +0800

    add part1 exercise 1.14*

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+
+const Anecdote = ({ anecdotes, votes, index, text }) => {
+  return (
+    <>
+      <h1>{text}</h1>
+      <div>{anecdotes[index]}</div>
+      <div>has {votes[index]} votes</div>
+    </>
+  );
+};
+
 const App = ({ anecdotes }) => {
   const votesInit = new Array(anecdotes.length).fill(0);
 
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(votesInit);
+  const [most, setMost] = useState(0);
 
-  const handleClick = () => {
+  const getMost = (votes) => {
+    const votesDesc = [...votes].sort((a, b) => b - a);
+    return votes.indexOf(votesDesc[0]);
+  };
+
+  const handleSelect = () => {
     const random = Math.floor(Math.random() * anecdotes.length);
     setSelected(random);
   };
@@ -15,15 +33,17 @@ const App = ({ anecdotes }) => {
   const handleVote = () => {
     const votesCopy = [...votes];
     votesCopy[selected]++;
+    const mostNew = getMost(votesCopy);
+    setMost(mostNew);
     setVotes(votesCopy);
   };
 
   return (
     <>
-      <div>{anecdotes[selected]}</div>
-      <div>has {votes[selected]} votes</div>
-      <button onClick={handleVote}>vote</button>
-      <button onClick={handleClick}>next anecdote</button>
+      <Anecdote anecdotes={anecdotes} votes={votes} index={selected} text={'Anecdote of the day'} />
+      <Button handleClick={handleVote} text={'vote'} />
+      <Button handleClick={handleSelect} text={'next anecdote'} />
+      <Anecdote anecdotes={anecdotes} votes={votes} index={most} text={'Anecdote with most votes'} />
     </>
   );
 };
