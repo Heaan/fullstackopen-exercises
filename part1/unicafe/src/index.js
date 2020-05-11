88d9327dd4c34b0614b106625a12d420d5447c17
Heaan <cxy.eml@live.cn>
2020-05-11 13:36:06 +0800

    add part1 exercise 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleSetGood = () => setGood(good + 1);
+  const handleSetNeutral = () => setNeutral(neutral + 1);
+  const handleSetBad = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={handleSetGood}>good</button>
+      <button onClick={handleSetNeutral}>neutral</button>
+      <button onClick={handleSetBad}>bad</button>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
