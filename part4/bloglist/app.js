c0ded8dcd209c0c1d9cf1fcfbaf144b0205aa849
Heaan <cxy.eml@live.cn>
2020-05-23 19:44:26 +0800

    add part4 exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config');
 const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
@@ -28,6 +29,7 @@ app.use(express.json());
 
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
